3c7bf0a914924c4ee844f38d2dc0a06fc21f6999
Amzshar <amzshar@gmail.com>
2023-07-10 20:54:02 +0800

    Create 20230710_allPrimesUpto.py

---
+++ 20230710_allPrimesUpto.py
@@ -0,0 +1,12 @@
+def allPrimesUpto(num):
+    primes = [2]
+    for number in range(3, num):
+        sqrtNum = number ** 0.5
+        for factor in primes:
+            if number % factor == 0:
+                break
+            if factor > sqrtNum:
+                primes.append(number)
+                break
+    return primes
+print(allPrimesUpto(100))
